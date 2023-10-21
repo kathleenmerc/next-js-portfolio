f7d77d8dc26721d0e56e3d83fe6f54f8f06ee95b
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-20 17:48:26 -0700

    style: fix navbar background height and spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 
       {/* white navbar background */}
         <motion.div 
-          className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h[3.25rem] sm:w-[36rem] sm:rounded-full"
+          className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
 
           /* Bounce effect */
           initial={{ y: -100, x: "-50%", opacity: 0 }}
@@ -30,7 +30,7 @@ export default function Header() {
                   animate={{ y: 0, opacity: 1 }}
                 >
                   <Link 
-                    className="flex w-full items-center justify-center px-3 py-5  hover:text-gray-950 transition" 
+                    className="flex w-full items-center justify-center px-3 py-3  hover:text-gray-950 transition" 
                     href={link.hash}
                   >
                     {
