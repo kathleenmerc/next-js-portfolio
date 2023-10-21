db01deb4811197ec0fab7cc48e82be5801e3780d
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-20 19:35:19 -0700

    feat: add intro section links

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import Image from 'next/image'
 import profilePhotoImg from '@/public/profilePhotoImg.png'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { FaArrowRight, FaArrowUpRightFromSquare, FaLinkedin, FaGithub } from 'react-icons/fa6'
 
 export default function Intro() {
   return (
@@ -30,7 +32,7 @@ export default function Intro() {
         </div>
 
         <motion.p 
-            className='flex items-center justify-center text-center mb-10 mt-4 text-lg font-medium text-gray-950 sm:text-xl sm:mt-0 sm:py-3'
+            className='flex items-center justify-center text-center mb-4 mt-4 text-lg font-medium text-gray-950 sm:text-xl sm:mt-0 sm:mb-0 sm:py-3'
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}    
         >
@@ -38,6 +40,35 @@ export default function Intro() {
             I'm a physical therapist turned software engineer 👩🏻‍⚕️👩🏻‍💻<br />
             Let's connect! 🤝<br />
         </motion.p>
+
+        <motion.div 
+            className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.1,
+            }}  
+        >
+            <Link 
+                href="#contact" 
+                className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full'
+            >
+                Contact <FaArrowRight />
+            </Link>
+
+            <a href="resume" target="_blank" className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+                Resume <FaArrowUpRightFromSquare />
+            </a>
+
+            <a href="link" target="_blank" className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+                <FaLinkedin />
+            </a>
+
+            <a href="link" target="_blank" className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+                <FaGithub />
+            </a>
+
+        </motion.div>
     </section>
   )
 }
