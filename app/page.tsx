bfdf405c4accd30247d8c617e3b565afb1ca25f9
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-23 12:36:10 -0700

    feat: create projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
       <About />
+      <Projects />
     </main>
   )
 }
---
+++ components/Projects.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import SectionHeading from './SectionHeading';
+
+export default function Projects() {
+  return (
+    <section>
+        <SectionHeading>Projects</SectionHeading>
+
+
+    </section>
+  )
+}
