e6fba795f8e288db139474916eafcacd2c50f054
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-23 19:27:54 -0700

    feat: add animation to projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import React from 'react';
 import SectionHeading from './SectionHeading';
 import { projectsData } from '@/lib/data';
 import Project from './Project';
+import { motion } from 'framer-motion';
 
 export default function Projects() {
     return (
-        <section 
+        <motion.section 
             className='mb-20 leading-8 sm:mb-30 scroll-mt-28'
             id='projects'
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.175 }}
         >
             <SectionHeading>Projects</SectionHeading>
 
@@ -20,7 +26,7 @@ export default function Projects() {
                 ))}
             </div>
 
-        </section>
+        </motion.section>
     )
 }
 
