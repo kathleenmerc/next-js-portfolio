c7692c2ec89ed7602f4d2ad762627e661e4138b3
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-24 15:55:02 -0700

    feat: install clsx for conditional rendering and react-intersection-observer for monitoring components in the viewport, implemented useState to track active section in navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export default function Header() {
+  {/* navbar active section */}
+  const [activeSection, setActiveSection] = useState('Home')
+
+
+
   return (
     <header className="z-[999] relative">
 
@@ -24,14 +30,17 @@ export default function Header() {
             {
               links.map(link => (
                 <motion.li 
-                  className="h-3/4 flex items-center justify-center relative" 
+                  className="h-3/4 flex items-center justify-center" 
                   key={link.hash}
                   initial={{ y: -100, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                 >
                   <Link 
-                    className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" 
+                    className={clsx("flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition", 
+                              {"text-gray-950": activeSection === link.name,}
+                    )}
                     href={link.hash}
+                    onClick={() => setActiveSection(link.name)}
                   >
                     {
                       link.name
