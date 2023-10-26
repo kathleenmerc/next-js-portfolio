39cef7ce101c2984763739dce40b8bc59bae617f
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-26 13:54:01 -0700

    chore: add more projects to project section

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,8 @@ import helicopterGameImg from "@/public/helicopterGameImg.png";
 import jeopardyImg from "@/public/jeopardyImg.png";
 import recipeFinderImg from "@/public/recipeFinderImg.png";
 import therapeaseImg from "@/public/therapeaseImg.png";
+import coffeeCornerImg from "@/public/coffeeCornerImg.png";
+import portfolioImg from "@/public/portfolioImg.png";
 
 export const links = [
   {
@@ -31,7 +33,7 @@ export const projectsData = [
     title: "Recipe Finder",
     description:
       "A website to help you find what recipes you can make with the ingredients you already have in your kitchen.",
-    tags: ["React", "Node.js", "MongoDB", "Express", "JavaScript", "CSS3", "JSON Web Token"],
+    tags: ["React", "Node.js", "MongoDB", "Express", "JavaScript", "CSS3", "JSON Web Token", "Render"],
     imageUrl: recipeFinderImg,
     liveLink: "https://recipe-finder.onrender.com/",
     repoLink: "https://github.com/kathleenmerc/recipe-finder",
@@ -40,7 +42,7 @@ export const projectsData = [
     title: "The Helicopter Game",
     description:
       "Recreated the classic Helicopter Game with new features such as 1 or 2-player modes, player lives, and high score functionality.",
-    tags: ["Vanilla JavaScript", "HTML5", "CSS3", "HTML Canvas"],
+    tags: ["Vanilla JavaScript", "HTML5", "CSS3", "HTML Canvas", "GitHub Pages"],
     imageUrl: helicopterGameImg,
     liveLink: "https://kathleenmerc.github.io/helicopter-game",
     repoLink: "https://github.com/kathleenmerc/helicopter-game",
@@ -49,7 +51,7 @@ export const projectsData = [
     title: "Jeopardy Game",
     description:
       "Jeopardy trivia game built with React and deployed on Netlify.",
-    tags: ["React", "JavaScript", "CSS3"],
+    tags: ["React", "JavaScript", "CSS3", "Netlify"],
     imageUrl: jeopardyImg,
     liveLink:"https://jeopardy-game-km.netlify.app/",
     repoLink: "https://github.com/kathleenmerc/react-jeopardy",
@@ -58,10 +60,28 @@ export const projectsData = [
     title: "TherapEase",
     description:
       "An app to enhance the appointment scheduling process for patients and physical therapists.",
-    tags: ["TypeScript", "Next.js", "Google Firebase & Firestore", "TailwindCSS", "Figma"],
+    tags: ["TypeScript", "Next.js", "Google Firebase & Firestore", "Tailwind CSS", "Figma"],
     imageUrl: therapeaseImg,
     liveLink: "https://vimeo.com/877328348?share=copy",
     repoLink: "https://github.com/chingu-voyages/v44-tier3-team-40"
+  },
+  {
+    title: "Coffee Corner",
+    description:
+      "A website for a cafe, with responsive designs for desktop and mobile devices.",
+    tags: ["HTML", "CSS", "Bootstrap", "GitHub Pages"],
+    imageUrl: coffeeCornerImg,
+    liveLink: "https://kathleenmerc.github.io/coffee-corner-website/",
+    repoLink: "https://github.com/kathleenmerc/coffee-corner-website"
+  },
+  {
+    title: "Portfolio",
+    description:
+      "My personal portfolio website to share my projects, skills, and contact information.",
+    tags: ["TypeScript", "Next.js", "Tailwind CSS", "Framer Motion", "Vercel" ],
+    imageUrl: portfolioImg,
+    liveLink: "#",
+    repoLink: "https://github.com/kathleenmerc/next-js-portfolio" 
   },
 ] as const;
 
@@ -73,7 +93,7 @@ export const skillsData = [
   "React",
   "Next.js",
   "Bootstrap",
-  "TailwindCSS",
+  "Tailwind CSS",
   "WordPress",
   "Figma",
   "Node.js",
