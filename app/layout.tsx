764d35bffa4dfb7172f5b8187e73d959cbc6b5e4
Kathleen Mercado <kathleenmerc@gmail.com>
2023-10-25 23:17:05 -0700

    refactor: create theme context provider file, add theme context provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header';
 import ActiveSectionContextProvider from '@/context/ActiveSectionContextProvider';
 import Footer from '@/components/Footer';
 import ThemeSwitch from '@/components/ThemeSwitch';
+import ThemeContextProvider from '@/context/ThemeContextProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,17 +20,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='!scroll-smooth'>
-      <body className={`${inter.className} bg-slate-200 text-slate-950 relative h-100 pt-38 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
+      <body className={`${inter.className} bg-slate-200 text-slate-950 relative h-100 mx-10 pt-38 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         {/* background styles */}
         <div className="bg-[#f3e2fb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#8CABFF]"></div>
         <div className="bg-[#d7f0fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:legt-[-33rem] lg:left=[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#37306B]"></div>
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
-        </ActiveSectionContextProvider>
-
-        <ThemeSwitch />
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   )
--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,45 +1,14 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BsMoon, BsSun } from 'react-icons/bs';
-
-
-export type Theme = "light" | "dark"
+import { useTheme } from "@/context/ThemeContextProvider";
 
 
 export default function ThemeSwitch() {
 
-    const [theme, setTheme] = useState<Theme>("light")
-
-    const toggleTheme = () => {
-        if (theme === "light") {
-            setTheme("dark")
-            window.localStorage.setItem("theme", "dark")
-            document.documentElement.classList.add("dark")
-        } else {
-            setTheme("light")
-            window.localStorage.setItem("theme", "light")
-            document.documentElement.classList.remove("dark")
-        }
-    }
-
-    /* set theme according to local storage or according to user's computer preference */
-    useEffect(() => {
-        const localTheme = window.localStorage.getItem("theme") as Theme | null
-        
-        if(localTheme) {
-            setTheme(localTheme)
-
-            if (localTheme === "dark") {
-                document.documentElement.classList.add("dark")
-            }
-        } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
-            setTheme("dark")
-            document.documentElement.classList.add("dark")
-        } 
-        
-    }, [])
-
+    const { theme, toggleTheme } = useTheme();
+    
     return (
         <button
             className='fixed bottom-5 right-5 bg-yellow-500 w-[3rem] h-[3rem] backdrop-blur-[0.5rem] border border-white/40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-blue-950'
